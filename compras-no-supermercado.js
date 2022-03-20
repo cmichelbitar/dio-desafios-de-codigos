f25dc56a39ce84efcbc4736cdeec9d3a4f3df7cb
cmichelbitar <github@carlosmichel.com.br>
2022-03-19 23:09:26 -0300

    adicionando compras no supermercado

---
+++ compras-no-supermercado.js
@@ -0,0 +1,21 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados
+
+// Complete os espaços em branco para a possível solução dos problemas
+
+let num = parseInt(gets());
+let index = 0;
+
+let listas = Array(num);
+let listaSet = Array(num);
+
+while (index < num ) {
+    listas[index] = gets().split(" ");
+    index++;
+}
+
+listas.map((i,d) => listaSet[d] = new Set(i));
+listaSet.map((i,d) => listas[d] = [...i]);
+
+listasString = listas.join();
+console.log(listaSet);
+//console.log(listasString.replace(/[,]/g, " "));
